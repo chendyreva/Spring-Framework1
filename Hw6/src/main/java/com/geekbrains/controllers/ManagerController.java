dcd0a77d673fc06fa8ac3668657eef9b4305e84d
chendyreva <nat-217@mail.ru>
2019-05-22 13:42:39 +0500

    security

---
+++ Hw6/src/main/java/com/geekbrains/controllers/ManagerController.java
@@ -0,0 +1,15 @@
+package com.geekbrains.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/manager")
+public class ManagerController {
+    @GetMapping("")
+    public String managerHome() {
+        return "manager-panel";
+    }
+}
+
